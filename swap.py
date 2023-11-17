2b20d5f2298e417ba1c5ca6c405e4ac91f7eeca8
26-PM <121531128+26-PM@users.noreply.github.com>
2023-11-17 05:03:14 +0000

    corrected

--- swap.py
+++ swap.py
@@ -3,7 +3,7 @@ for i in range(6):
     number = int(input("Enter a number: "))
     my_list.append(number)
 
-if((my_list[0]+my_list[1]+my_list[2])>(my_list[3]+my_list[2]+my_list[1])):
+if((my_list[0]+my_list[1]+my_list[2])>(my_list[3]+my_list[4]+my_list[5])):
     my_list.sort()
 
 print(my_list)
