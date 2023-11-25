efb9fd77c02f4962286d42a7e9871ae88b0e2dd4
26-PM <121531128+26-PM@users.noreply.github.com>
2023-11-25 12:38:11 +0000

    keysValuesPairs

---
+++ keysValuesPairs.py
@@ -0,0 +1,15 @@
+# which function will you use to fetch all keys, all values, and key value pairs from a given dictionary?
+
+dictionary={
+    1:"A",
+    2:"B",
+    3:"C",
+    4:"D"
+}
+keys=dictionary.keys()
+values=dictionary.values()
+keyValuePairs=dictionary.items()
+
+print("KEYS = dictionary.keys() =",keys)
+print("VALUES = dictionary.values() =",values)
+print("KEY VALUE PAIRS = dictionary.items() =",keyValuePairs)
